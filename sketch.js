5bf807ffc864f7f038f8749a4e883bad751b8c07
Lavya321 <72295531+Lavya321@users.noreply.github.com>
2020-10-10 16:09:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var movingrect,fixedrect; 
+function setup() {
+  createCanvas(800,400);
+ movingrect= createSprite(400, 200, 100, 50);
+ fixedrect=createSprite(200,200,50,150);
+ movingrect.shapeColor="purple";
+ fixedrect.shapeColor="yellow";
+  
+}
+
+
+
+function draw() {
+  background(255,255,255);
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+  if (movingrect.x-fixedrect.x < movingrect.width/2+fixedrect.width/2 && 
+    fixedrect.x-movingrect.x < movingrect.width/2+fixedrect.width/2 && movingrect.y-fixedrect.y < movingrect.height/2+fixedrect.height/2 && 
+    fixedrect.y-movingrect.y < movingrect.height/2+fixedrect.height/2  ) {
+    movingrect.shapeColor="yellow";
+  } else {
+    movingrect.shapeColor="purple";
+ fixedrect.shapeColor="yellow";
+  }
+  drawSprites();
+  
+}
